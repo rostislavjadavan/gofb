710e4d8ea081bde2b9ff7192ce9259f20bb93fab
Rostislav Jadavan <rostislav@jadavan.com>
2020-04-15 21:52:46 +0200

    keyboard input

--- window.go
+++ window.go
@@ -8,15 +8,18 @@ import (
 )
 
 type Window struct {
-	width      int
-	height     int
-	fullscreen bool
+	Width      int
+	Height     int
+	Fullscreen bool
 	window     *glfw.Window
+	running    bool
 
 	lastFrameTime   time.Time
 	frameDeltaMs    int64
 	globalElapsedMs int64
-	framesCount		int64
+	framesCount     int64
+
+	keyPress []bool
 }
 
 // NewWindow create new OpenGL window
@@ -46,10 +49,12 @@ func NewWindow(name string, width int, height int, fullscreen bool) *Window {
 		panic(err)
 	}
 
-	w := Window{width: width, height: height, fullscreen: fullscreen, window: window}
+	w := Window{Width: width, Height: height, Fullscreen: fullscreen, window: window}
 	w.set2DProjection()
 	w.lastFrameTime = time.Now()
+	w.keyPress = make([]bool, 1024)
 	window.SetKeyCallback(w.keyCallback)
+	w.running = true
 
 	return &w
 }
@@ -76,22 +81,26 @@ func (w *Window) FinalizeFrame() {
 }
 
 // GetDeltaTime return number of milliseconds elapsed when rendering last frame
-func (w *Window) GetDeltaTimeMs() int64  {
+func (w *Window) GetDeltaTimeMs() int64 {
 	return w.frameDeltaMs
 }
 
 // GetTotalElapsedMs return number of milliseconds elapsed since application started
-func (w *Window) GetTotalElapsedMs() int64  {
+func (w *Window) GetTotalElapsedMs() int64 {
 	return w.globalElapsedMs
 }
 
 func (w *Window) GetFPS() float32 {
-	return float32(w.framesCount) / float32(w.globalElapsedMs) * 1000;
+	return float32(w.framesCount) / float32(w.globalElapsedMs) * 1000
+}
+
+func (w *Window) IsKey(key glfw.Key) bool {
+	return w.keyPress[key]
 }
 
 // IsRunning check if application is running
 func (w *Window) IsRunning() bool {
-	return !w.window.ShouldClose()
+	return !w.window.ShouldClose() && w.running
 }
 
 // Destroy will close window
@@ -99,8 +108,19 @@ func (w *Window) Destroy() {
 	defer glfw.Terminate()
 }
 
+func (w *Window) Stop() {
+	w.running = false
+}
+
 func (w *Window) keyCallback(window *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
-	// TODO
+	switch action {
+	case glfw.Repeat:
+	case glfw.Press:
+		w.keyPress[key] = true
+		break
+	default:
+		w.keyPress[key] = false
+	}
 }
 
 func (w *Window) set2DProjection() {
@@ -109,7 +129,7 @@ func (w *Window) set2DProjection() {
 
 	gl.MatrixMode(gl.PROJECTION)
 	gl.LoadIdentity()
-	gl.Ortho(0, float64(w.width), float64(w.height), 0, 0, 1)
+	gl.Ortho(0, float64(w.Width), float64(w.Height), 0, 0, 1)
 
 	gl.MatrixMode(gl.MODELVIEW)
 	gl.LoadIdentity()
